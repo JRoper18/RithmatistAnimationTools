6c2e3ed97ea8f830f05d61c3e1b8e322c0f6a3f2
UnusualArnen <jack@skroper.com>
2016-08-17 20:20:35 -0600

    FUCK YEAH Transparency using flood fill works

--- removeTransparency.py
+++ removeTransparency.py
@@ -2,13 +2,33 @@ from PIL import Image
 import os
 import sys
 
+def checkSurroundingPixels(pixel, image, color):
+    matchedPixels = [];
+
+
+
+
 workingDir = os.getcwd()
 for filename in os.listdir(workingDir):
-    if filename.endswith(".jpg") || filename.endswith("png"):
+    if filename.endswith(".jpg") or filename.endswith("png") or filename.endswith("gif"):
         image = Image.open(filename)
-        pixels = image.load();
-        #for each jpg file in directory, open it in the image variable, load pixels.
+        #for each jpg or png file in directory, open it in the image variable, load pixels.
         print(image.format, image.size, image.mode)
-        savedPixels = [0,0]
-        for x in image.size[0]*imageSize[1]: #Hit every pixel if neccesary
-            
+        colorToCheck = image.getpixel((0,0))
+        print(colorToCheck);
+        pixelStack = [(0,0)]
+        while(len(pixelStack) > 0):
+            currentPixel = pixelStack.pop();
+            if image.getpixel(currentPixel) == colorToCheck:
+                image.putpixel(currentPixel, (0,0,0,0))
+                if currentPixel[0] < image.size[0]-1:
+                    #Check if out of bounds
+                    pixelStack.append((currentPixel[0]+1, currentPixel[1]))
+                if currentPixel[1] < image.size[1]-1:
+                    pixelStack.append((currentPixel[0], currentPixel[1] + 1))
+                if currentPixel[0] > 0:
+                    pixelStack.append((currentPixel[0]-1, currentPixel[1]))
+                if currentPixel[1] > 0:
+                    pixelStack.append((currentPixel[0], currentPixel[1] - 1))
+
+        image.save(filename);
